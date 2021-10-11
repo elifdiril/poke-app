55be4f1557627592b442296242f92743eb20a8aa
elif <elifdiril6@gmail.com>
2021-10-11 14:31:27 +0300

    some styles added

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -1,4 +1,5 @@
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import '../App.css';
 
 function DetailModal({ modal, toggle, data, loading, error }) {
     return (
@@ -8,7 +9,9 @@ function DetailModal({ modal, toggle, data, loading, error }) {
                 {loading && "Loading..."}
                 {error && "Error " + error}
                 {data && data.pokemon.evolutions && data.pokemon.evolutions.length > 0 && data.pokemon.evolutions.map((item, id) => (
-                    <img key={id} src={item.image} alt="" className="item-img" />
+                    <div className="detail-item"><img key={id} src={item.image} alt="" className="item-img" />
+                        <div className="detail-name">{item.name}</div>
+                    </div>
                 ))
                 }
                 {data && !data.pokemon.evolutions
